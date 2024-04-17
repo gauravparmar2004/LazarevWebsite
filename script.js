03e547f216126f79ebaed9e981b5f0fa2859450b
Gaurav <gaurav.1222parmar@gmail.com>
2024-04-17 18:19:48 +0530

    add some pages

--- script.js
+++ script.js
@@ -35,9 +35,53 @@ function navAnimation() {
         })
     })
 }
-// navAnimation()
 
-var elem=document.querySelectorAll(".right-elem")
-elem.forEach(function(){
-    console.log("Hellooo")
-})
+function page2Animation() {
+    var elem = document.querySelectorAll(".right-elem")
+    elem.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1,
+            })
+        })
+
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0,
+            })
+            elem.addEventListener("mousemove", function (details) {
+                gsap.to(elem.childNodes[3], {
+                    x: details.x - elem.getBoundingClientRect().x - 50,
+                    y: details.y - elem.getBoundingClientRect().y - 150,
+                })
+            })
+        })
+    })
+}
+
+function page3VideoAnimation() {
+    var page3Center = document.querySelector(".page3-center")
+    var video = document.querySelector("#page3 video")
+
+    page3Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0,
+        })
+    })
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px",
+        })
+    })
+}
+page2Animation()
+navAnimation()
+page3VideoAnimation()
