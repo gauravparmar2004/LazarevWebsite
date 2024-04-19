81c3aefc7f0b50d4166f143ac16025bef502d60c
Gaurav <gaurav.1222parmar@gmail.com>
2024-04-19 14:53:40 +0530

    add some pages

--- script.js
+++ script.js
@@ -85,3 +85,15 @@ function page3VideoAnimation() {
 page2Animation()
 navAnimation()
 page3VideoAnimation()
+var sections = document.querySelectorAll(".sec-right")
+sections.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()
+    })
+
+    elem.addEventListener("mouseleave", function () {
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()
+    })
+})
