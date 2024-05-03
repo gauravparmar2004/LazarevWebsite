d319db445f55ca27d962d8bae44c7a2f824ae3ce
Gaurav <gaurav.1222parmar@gmail.com>
2024-05-03 23:36:10 +0530

    add some pages

--- script.js
+++ script.js
@@ -1,108 +1,107 @@
 function navAnimation() {
-    let nav = document.querySelector("#nav")
+    let nav = document.querySelector("#nav");
     nav.addEventListener("mouseenter", function () {
-        let tl = gsap.timeline()
+        let tl = gsap.timeline();
         tl.to("#nav-bottom", {
             height: "23vh",
-        })
+        });
         tl.to("#nav h5", {
-            display: "block"
-        })
+            display: "block",
+        });
 
         tl.from("#nav h5 span", {
             y: 25,
             stagger: {
                 amount: 0.3,
-            }
-        })
-    })
+            },
+        });
+    });
 
     nav.addEventListener("mouseleave", function () {
-        let tl = gsap.timeline()
+        let tl = gsap.timeline();
         tl.to("#nav h5 span", {
             y: 25,
             stagger: {
                 amount: 0.2,
-            }
-        })
+            },
+        });
         tl.to(".nav-part2 h5", {
             display: "none",
             duration: 0.1,
-        })
+        });
         tl.to("#nav-bottom", {
             height: 0,
             duration: 0.1,
-        })
-    })
+        });
+    });
 }
 
 function page2Animation() {
-    var elem = document.querySelectorAll(".right-elem")
+    var elem = document.querySelectorAll(".right-elem");
     elem.forEach(function (elem) {
         elem.addEventListener("mouseenter", function () {
             gsap.to(elem.childNodes[3], {
                 opacity: 1,
                 scale: 1,
-            })
-        })
+            });
+        });
 
         elem.addEventListener("mouseleave", function () {
             gsap.to(elem.childNodes[3], {
                 opacity: 0,
                 scale: 0,
-            })
+            });
             elem.addEventListener("mousemove", function (details) {
                 gsap.to(elem.childNodes[3], {
                     x: details.x - elem.getBoundingClientRect().x - 50,
                     y: details.y - elem.getBoundingClientRect().y - 150,
-                })
-            })
-        })
-    })
+                });
+            });
+        });
+    });
 }
 
 function page3VideoAnimation() {
-    var page3Center = document.querySelector(".page3-center")
-    var video = document.querySelector("#page3 video")
+    var page3Center = document.querySelector(".page3-center");
+    var video = document.querySelector("#page3 video");
 
     page3Center.addEventListener("click", function () {
-        video.play()
+        video.play();
         gsap.to(video, {
             transform: "scaleX(1) scaleY(1)",
             opacity: 1,
             borderRadius: 0,
-        })
-    })
+        });
+    });
     video.addEventListener("click", function () {
-        video.pause()
+        video.pause();
         gsap.to(video, {
             transform: "scaleX(0.7) scaleY(0)",
             opacity: 0,
             borderRadius: "30px",
-        })
-    })
+        });
+    });
 }
 
 function page7SectionVideoAnimation() {
-    var sections = document.querySelectorAll(".sec-right")
+    var sections = document.querySelectorAll(".sec-right");
     sections.forEach(function (elem) {
         elem.addEventListener("mouseenter", function () {
-            elem.childNodes[3].style.opacity = 1
-            elem.childNodes[3].play()
-        })
+            elem.childNodes[3].style.opacity = 1;
+            elem.childNodes[3].play();
+        });
 
         elem.addEventListener("mouseleave", function () {
-            elem.childNodes[3].style.opacity = 0
-            elem.childNodes[3].load()
-        })
-    })
+            elem.childNodes[3].style.opacity = 0;
+            elem.childNodes[3].load();
+        });
+    });
 }
 
-page2Animation()
-navAnimation()
-page3VideoAnimation()
-page7SectionVideoAnimation()
-
+page2Animation();
+navAnimation();
+page3VideoAnimation();
+page7SectionVideoAnimation();
 
 // var secsections = document.querySelectorAll(".box1")
 // secsections.forEach(function (elem) {
